15683f8ab72e10832a7807dc5b53a0463e17414e
Brett Goss <brettgoss@live.com>
2016-10-11 10:19:20 -0700

    created local test server

---
+++ day2/http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) =>{
+  console.log("Path:", req.url, "Method:", req.method);
+  var message;
+  if (req.url === '/'){
+    res.statusCode = 200;
+    message = "Welcome to my localhost!";
+  } else if (req.url === '/about') {
+    res.statusCode = 200;
+    message = "About Me!";
+  } else {
+    res.statusCode = 404;
+    message = "File Not Found";
+  }
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(message + '\n');
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
