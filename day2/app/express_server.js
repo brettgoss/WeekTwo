9c833ece70f429cfc1b8680fb246f1fde5257593
Brett Goss <brettgoss@live.com>
2016-10-13 20:10:02 -0700

    Added login/logout functionality

--- day2/app/express_server.js
+++ day2/app/express_server.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 // Declare constants used throughout file
 const app = express();
+
 
 // Configuration
 app.set("view engine", "ejs");
@@ -20,22 +22,44 @@ var urlDatabase = {
 };
 
 // Middleware
+app.use(express.static(__dirname + '/assets'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: false
-}));
-app.use(express.static(__dirname + '/assets'));
+}))
 
 // app.use(methodOverride('_method'))
 
 
 // Routes
 
-  //Index
+// Index
 app.get("/", (req, res) => {
-  res.render('index');
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    urls: urlDatabase,
+    login: req.cookies['user']
+  };
+  res.render('index', templateVars);
+})
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    login: req.cookies['user']
+  };
+  res.render("urls_index", templateVars);
+})
+// app.get("/urls/new", (req, res) => {
+//   res.render("urls_new", {username: req.cookies["username"]});
+// })
+
+app.post("/login", (req, res) => {
+  let username = req.body.username
+  res.cookie("user",username)
+  res.redirect("/urls")
+})
+app.post("/logout", (req, res) => {
+  delete res.clearCookie("user")
+  res.redirect("/urls")
 })
 
 app.post("/u", (req, res) => {
@@ -47,28 +71,25 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 })
+app.post("/urls/:id/edit", (req, res) => {
+  console.log(req.body.edit)
+  urlDatabase[req.params.id] = req.body.edit
+  res.redirect('/urls/')
+})
 
-app.get("/urls", (req, res) => {
-  console.log(urlDatabase)
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
 app.get("/u/:id", (req, res) => {
-  //let templateVars = { shortURL: req.params.id };
-  //res.render("urls_show", templateVars);
   let longURL = urlDatabase[req.params.id];
   console.log(urlDatabase[req.params.id])
   res.redirect(longURL)
 });
 app.post("/urls/:id", (req, res) => {
   //console.log(res.body)
-  var key = req.params.id
-  res.render("urls_show", {urls:urlDatabase, key: key} )
-})
-app.post("/urls/:id/edit", (req, res) => {
-  console.log(req.body.edit)
-  urlDatabase[req.params.id] = req.body.edit
-  res.redirect(`/urls/`)
+  let templateVars = {
+    urls: urlDatabase,
+    login: req.cookies['user'],
+    key: req.params.id
+  };
+  res.render("urls_show", templateVars )
 })
 
 
